bd10aa359621f956788bb0119a7fc1025148a11a
charlyromytanga <charlyromytanga.cytech@gmail.com>
2026-03-05 09:02:34 +0100

    Function merge des deux prédiction mise en place

--- Repo/prod_api_rest/repository.py
+++
@@ -1,9 +0,0 @@
-from server import Dataset
-
-
-class Repository:
-    def get_client_data(self,index):
-        dataset = Dataset()
-        data_dict = dataset.load_data(file_pat='train.csv')
-        res_data = data_dict[index]
-        return res_data
